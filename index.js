c2eb2042d924864fabbb21930a2e8f5043c57dca
Joshua Scott <scottjm@nsuok.edu>
2023-04-14 09:28:59 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+// zero = document.getElementById("zero")
+// one = document.getElementById("one")
+// two = document.getElementById("two")
+// three = document.getElementById("three")
+// four = document.getElementById("four")
+// five = document.getElementById("five")
+// six = document.getElementById("six")
+// seven = document.getElementById("seven")
+// eight = document.getElementById("eight")
+// nine = document.getElementById("nine")
+// divide = document.getElementById("divide")
+// multiply = document.getElementById("multiply")
+// plus = document.getElementById("plus")
+// minus = document.getElementById("minus")
+// equal = document.getElementById("equal")
+// decimal = document.getElementById("decimal")
+// clear = document.getElementById("clear")
+// deleteButton = document.getElementById("delete")
+let countOne = 0
+let countTwo = 0
+let displayNumber =""
+let sign = ""
+let output = document.getElementById("calc-output")
+
+function sum(){ 
+   
+    sign = "+"
+    output.textContent = sign
+}
+
+function total(){
+   
+    sign = "="
+    output.textContent = sign
+
+}
+function zero(){
+zero = count;
+output.textContent = zero}
+
+function one() {
+    let one = 1
+    countOne = one
+    output.textContent += countOne
+}
